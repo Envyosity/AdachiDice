c2739976f018982b387aefba1fb2768a341fe258
Leonardo Krunitzky <110630098+Envyosity@users.noreply.github.com>
2025-04-17 09:50:43 -0300

    heyheyhey

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function rollTheDice() {
   const container = document.getElementById("form");
   const containerImg = document.getElementById("tv");
   const image = document.getElementById("heyheyhey");
-  const audio = new Audio(`/sounds/${parseInt(Math.random() * 2)}.mp3`);
+  const audio = new Audio(`./sounds/${parseInt(Math.random() * 2)}.mp3`);
   
-  image.src = `/imgs/${parseInt(Math.random() * 10)}.png`;
+  image.src = `./imgs/${parseInt(Math.random() * 10)}.png`;
   container.style.display = "none"
   containerImg.style.display = "flex";
 
